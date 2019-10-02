a151a446a836abfa849834007da31e01708901fd
Zsolt Nagy <Zsolt.Nagy1@ibm.com>
2019-10-02 11:48:09 +0200

    Adding_jupiter_notebook

---
+++ Code/.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## <span style="color:purple">Coursera Capstone project for Applied Data Science</span>
+
+# #### Importing packages
+
+# In[ ]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print('Hello Capstone Project Course!')
+
