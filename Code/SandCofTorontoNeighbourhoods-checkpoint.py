bd524eafc46157142455997e76396105019ac4ce
Zsolt Nagy <Zsolt.Nagy1@ibm.com>
2019-10-10 11:03:01 +0200

    Update_Notebook_Preparing_data_for_clustering

--- Code/SandCofTorontoNeighbourhoods-checkpoint.py
+++ Code/SandCofTorontoNeighbourhoods-checkpoint.py
@@ -5,7 +5,7 @@
 
 # ### <span style="color:darkred">Importing packages</span>
 
-# In[1]:
+# In[3]:
 
 
 # Importing general packages
@@ -32,8 +32,11 @@ from bs4 import BeautifulSoup
 get_ipython().system('conda install -c conda-forge geocoder --yes ')
 import geocoder
 
+# Packages for hiding sensitive data
+from IPython.display import HTML
 
-# In[2]:
+
+# In[4]:
 
 
 html = requests.get('https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canada:_M')
@@ -42,7 +45,7 @@ html = requests.get('https://en.wikipedia.org/wiki/List_of_postal_codes_of_Canad
 bs = BeautifulSoup(html.text, 'lxml')
 
 
-# In[3]:
+# In[5]:
 
 
 #Defining customized replace function
@@ -103,13 +106,13 @@ df_n = df_n.reset_index(drop=True)
 df_n
 
 
-# In[4]:
+# In[6]:
 
 
 print(df_n.shape)
 
 
-# In[5]:
+# In[7]:
 
 
 df_gsp = pd.read_csv('http://cocl.us/Geospatial_data')
@@ -117,9 +120,102 @@ df_gsp = df_gsp.rename(columns={"Postal Code": "Postcode"})
 df_gsp.head()
 
 
-# In[6]:
+# In[8]:
 
 
 df_n = df_n >> left_join(df_gsp, by = "Postcode")
 df_n
+
+
+# ### <span style="color:darkred"> Adding the most common venue categories in each neighborhood in Toronto </span>
+
+# In[16]:
+
+
+import getpass
+
+CLIENT_ID = getpass.getpass('Enter your Foursquare CLIENT_ID')
+CLIENT_SECRET = getpass.getpass('Enter your Foursquare CLIENT_SECRET')
+VERSION = '20180605'
+LIMIT = 100
+
+print('Your credentails are stored')
+
+
+# In[20]:
+
+
+def getNearbyVenues(names, latitudes, longitudes, radius=500):
+    
+    venues_list=[]
+    for name, lat, lng in zip(names, latitudes, longitudes):
+        #print(name)
+            
+        # create the API request URL
+        url = 'https://api.foursquare.com/v2/venues/explore?&client_id={}&client_secret={}&v={}&ll={},{}&radius={}&limit={}'.format(
+            CLIENT_ID, 
+            CLIENT_SECRET, 
+            VERSION, 
+            lat, 
+            lng, 
+            radius, 
+            LIMIT)
+            
+        # make the GET request
+        results = requests.get(url).json()["response"]['groups'][0]['items']
+        
+        # return only relevant information for each nearby venue
+        venues_list.append([(
+            name, 
+            lat, 
+            lng, 
+            v['venue']['name'], 
+            v['venue']['location']['lat'], 
+            v['venue']['location']['lng'],  
+            v['venue']['categories'][0]['name']) for v in results])
+
+    nearby_venues = pd.DataFrame([item for venue_list in venues_list for item in venue_list])
+    nearby_venues.columns = ['Neighborhood', 
+                  'Neighborhood Latitude', 
+                  'Neighborhood Longitude', 
+                  'Venue', 
+                  'Venue Latitude', 
+                  'Venue Longitude', 
+                  'Venue Category']
+    
+    return(nearby_venues)
+
+
+# ### <span style="color:darkred"> Filtering neighbourhoods that belongs to Toronto </span>
+
+# In[11]:
+
+
+df_n_tor = df_n >> mask(X.Borough.str.contains('Toronto') == True)
+df_n_tor
+
+
+# In[29]:
+
+
+df_Tor_venues = getNearbyVenues(df_n_tor['Neighbourhood'], df_n_tor['Latitude'], df_n_tor['Longitude'], 500)
+df_Tor_venues = df_Tor_venues.rename(columns={"Neighborhood Latitude": "Neighborhood_Latitude",
+                                             "Neighborhood Longitude": "Neighborhood_Longitude",
+                                             "Venue Latitude": "Venue_Latitude",
+                                             "Venue Longitude": "Venue_Longitude",
+                                             "Venue Category": "Venue_Category"})
+df_Tor_venues.head(10)
+
+
+# In[34]:
+
+
+df_Tor_venues >> group_by(X.Neighborhood) >> summarize(Count = X.Venue_Category.count())
+
+
+# In[48]:
+
+
+df_Tor_venues = df_Tor_venues >> group_by(X.Neighborhood, X.Venue_Category) >> summarize(Count = X.Venue_Category.count()) >> arrange(X.Neighborhood, X.Count, ascending=False) 
+df_Tor_venues
 
